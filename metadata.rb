674ecd801e5fe0d190570f16f4d9bd33ff994ae3
Dominic LoBue <dominic.lobue@gmail.com>
2015-05-11 13:34:47 -0700

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Dominic LoBue"
+maintainer_email "dominic.lobue@gmail.com"
+license          "Apache 2.0"
+description      "adds untar_archive resource"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README'))
+version          "0.0.1"
